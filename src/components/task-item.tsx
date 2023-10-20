486f487480a428ff825d91f25a0caad6a06d5c28
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-20 12:50:11 +0100

    add anim functions

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,10 +1,19 @@
 import React, { useCallback } from "react";
 import { Pressable } from "react-native";
-import { Box, useTheme, themeTools, useColorModeValue } from "native-base";
+import {
+  Box,
+  useTheme,
+  themeTools,
+  Text,
+  useColorModeValue,
+  HStack,
+  Theme,
+} from "native-base";
 import Checkbox from "./checkbox";
-
+import TaskLabel from "./task-label";
 interface Props {
   isDone: boolean;
+  onToggleCheckbox?: () => void;
 }
 
 const TaskItem = (props: Props) => {
@@ -13,6 +22,45 @@ const TaskItem = (props: Props) => {
   const highlightColor = themeTools.getColor(
     theme,
     useColorModeValue("blue.500", "blue.400"),
+  );
+  const boxStroke = themeTools.getColor(
+    theme,
+    useColorModeValue("mute.300", "mute.500"),
+  );
+  const checkMarkColor = themeTools.getColor(
+    theme,
+    useColorModeValue("white", "white"),
+  );
+  const activeTextColor = themeTools.getColor(
+    theme,
+    useColorModeValue("darkText", "lightText"),
+  );
+  const doneTextColor = themeTools.getColor(
+    theme,
+    useColorModeValue("mute.400", "mute.600"),
+  );
+
+  return (
+    <HStack>
+      {" "}
+      <Box width={30} height={40} mr={2}>
+        <Pressable onPress={onToggleCheckbox}>
+          <Checkbox
+            checkmarkColor={checkMarkColor}
+            highlightColor={highlightColor}
+            boxOutlineColor={boxOutlineColor}
+            checked={isDone}
+          />
+        </Pressable>
+        <TaskLabel
+          textColor={activeTextColor}
+          inactiveTextColor={doneTextColor}
+          strikeThrough={isDone}
+        >
+          Task Item
+        </TaskLabel>
+      </Box>
+    </HStack>
   );
 };
 
