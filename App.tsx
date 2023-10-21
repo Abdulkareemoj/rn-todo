e7a3a061411d6f9845c9b492a6952ff3c83af0bf
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-21 09:52:31 +0100

    change screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import AppContainer from "./src/components/app-container";
 import React from "react";
-import MainScreen from "./src/screens/main";
+import Navigator from "./src";
+
 export default function App(): JSX.Element {
   return (
     <AppContainer>
-      <MainScreen />
+      <Navigator />
     </AppContainer>
   );
 }
