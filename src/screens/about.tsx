a550fb493260b5e9f647eb2244afc33fdbcceb1c
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-22 22:01:15 +0100

    about screen

---
+++ src/screens/about.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Box, Text, VSstack } from "native-base";
+const AboutScreen = (): JSX.Element => {
+  return <Box>About</Box>;
+};
+
+export default AboutScreen;
