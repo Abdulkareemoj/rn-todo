5c8447603129bcd6da44b3e0c6ded29018482674
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-23 21:45:07 +0100

    add editing function

--- src/components/task-item.tsx
+++ src/components/task-item.tsx
@@ -1,5 +1,10 @@
 import React, { useCallback } from "react";
-import { Pressable } from "react-native";
+import { type PanGestureHandlerProps } from "react-native-gesture-handler";
+import {
+  type NativeSyntheticEvent,
+  Pressable,
+  type TextInputChangeEventData,
+} from "react-native";
 import {
   Box,
   useTheme,
@@ -7,22 +12,36 @@ import {
   useColorModeValue,
   HStack,
   Icon,
+  Input,
 } from "native-base";
 import Checkbox from "./checkbox";
 import TaskLabel from "./task-label";
 import SwipeView from "./swipe-view";
 import { feather } from "@expo/vector-icons";
-import { useColorMode } from "native-base";
+
 interface Props extends Pick<PanGestureHandlerProps, "simultaneousHandlers"> {
   isDone: boolean;
   onToggleCheckbox?: () => void;
   onPressLabel?: () => void;
   onRemove?: () => void;
   subject: string;
+  isEditing: boolean;
+  onChangeSubject: (subject: string) => void;
+  onFinishEditing?: () => void;
 }
 
 const TaskItem = (props: Props) => {
-  const { isDone, onToggleCheckbox, onPressLabel, onRemove, subject } = props;
+  const {
+    isDone,
+    onToggleCheckbox,
+    onPressLabel,
+    onRemove,
+    subject,
+    isEditing,
+    simultaneousHandlers,
+    onChangeSubject,
+    onFinishEditing,
+  } = props;
   const theme = useTheme();
   const highlightColor = themeTools.getColor(
     theme,
@@ -45,33 +64,74 @@ const TaskItem = (props: Props) => {
     useColorModeValue("mute.400", "mute.600"),
   );
 
+  const handleChangeSubject = useCallback(
+    (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+      onChangeSubject && onChangeSubject(e.nativeEvent.text);
+    },
+    [onChangeSubject],
+  );
+
   return (
-    <HStack
-      alignItems="center"
-      w="full"
-      px={4}
-      py={2}
-      bg={useColorModeValue("warmGray.50", "primary.900")}
-    >
-      {" "}
-      <Box width={30} height={40} mr={2}>
-        <Pressable onPress={onToggleCheckbox}>
-          <Checkbox
-            checkmarkColor={checkMarkColor}
-            highlightColor={highlightColor}
-            boxOutlineColor={boxOutlineColor}
-            checked={isDone}
-          />
-        </Pressable>
-        <TaskLabel
-          textColor={activeTextColor}
-          inactiveTextColor={doneTextColor}
-          strikeThrough={isDone}
+    <>
+      <SwipeView
+        simultaneousHandlers={simultaneousHandlers}
+        onSwipeLeft={onRemove}
+        backView={
+          <Box
+            w="full"
+            h="full"
+            bg="red.500"
+            alignItems="flex-end"
+            justifyItems="center"
+          >
+            <Icon color="white" as={<Feather name="trash-2" size="sm" />} />
+          </Box>
+        }
+      >
+        <HStack
+          alignItems="center"
+          w="full"
+          px={4}
+          py={2}
+          bg={useColorModeValue("warmGray.50", "primary.900")}
         >
-          Task Item
-        </TaskLabel>
-      </Box>
-    </HStack>
+          {" "}
+          <Box width={30} height={40} mr={2}>
+            <Pressable onPress={onToggleCheckbox}>
+              <Checkbox
+                checkmarkColor={checkMarkColor}
+                highlightColor={highlightColor}
+                boxOutlineColor={boxStroke}
+                checked={isDone}
+              />
+            </Pressable>
+          </Box>
+          {isEditing ? (
+            <Input
+              placeholder="Task"
+              value={subject}
+              variant="unstyled"
+              fontSize={20}
+              px={1}
+              py={0}
+              autoFocus
+              blurOnSubmit
+              onChange={handleChangeSubject}
+              onBlur={onFinishEditing}
+            />
+          ) : (
+            <TaskLabel
+              textColor={activeTextColor}
+              inactiveTextColor={doneTextColor}
+              strikeThrough={isDone}
+              onPress={onPressLabel}
+            >
+              {subject}
+            </TaskLabel>
+          )}
+        </HStack>
+      </SwipeView>
+    </>
   );
 };
 
