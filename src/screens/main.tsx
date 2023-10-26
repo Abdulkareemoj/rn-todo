64c295db81cc52c1bcc378fc017696b9063709c3
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-26 22:01:55 +0100

    trying nanoid

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -8,10 +8,21 @@ import {
   useColorMode,
   useColorModeValue,
 } from "native-base";
-import { Pressable } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import ThemeSwitch from "../components/theme-switch";
 import TaskItem from "../components/task-item";
+import { nanoid } from "nanoid";
 
+const initialData = [
+  {
+    id: nanoid(),
+    subject: "Exist",
+  },
+  {
+    id: nanoid(),
+    subject: "Eat",
+  },
+];
 export default function MainScreen(): JSX.Element {
   const [checked, setChecked] = useState(false);
   const [subject, setSubject] = useState("Task Item");
