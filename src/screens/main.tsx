66798d5349c06d52d548f9ae4a6c20667865129f
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-10-28 23:08:35 +0100

    add callbacks

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -12,24 +12,68 @@ import { AntDesign } from "@expo/vector-icons";
 import ThemeSwitch from "../components/theme-switch";
 import TaskItem from "../components/task-item";
 import { nanoid } from "nanoid";
-
+import TaskList from "../components/task-list";
 const initialData = [
   {
     id: nanoid(),
     subject: "Exist",
+    done: false,
   },
   {
     id: nanoid(),
     subject: "Eat",
+    done: false,
+  },
+  {
+    id: nanoid(),
+    subject: "Sleep",
+    done: false,
   },
 ];
-export default function MainScreen(): JSX.Element {
-  const [checked, setChecked] = useState(false);
-  const [subject, setSubject] = useState("Task Item");
-  const [isEditing, setEditing] = useState(false);
-  const handlePressCheckbox = useCallback(() => {
-    setChecked((prev: boolean = false) => !prev);
+
+export default function MainScreen(): JSX.Element   {
+  const [data, setData] = useState(initialData);
+  const [editingItemId, setEditingItemId] = useState<string | null>(null);
+
+  const handleToggleTaskItem = useCallback((item) => {
+    setData((prevData) => {
+      const newData = [...prevData];
+      const index = prevData.indexOf(item);
+      newData[index] = {
+        ...item,
+        done: !item.done
+      }
+      return newData
+    });
   }, []);
+
+ const handleChangeTaskItemSubject = useCallback((item, newSubject) => {
+  setData(prevData) => {
+const newData = [...prevData]
+const index = prevData.indexOf(item)
+      newData[index] = {
+        ...item,
+        subject: newSubject
+      }
+      return newData
+    })
+ }, [])
+
+ const handleFinishEditingTaskItem = useCallback(_item=> {setEditingitemId(null)
+
+}, [])
+
+ const handlePressTaskItemLabel = useCallback(item => {
+setEditingItemId(item.id)
+ }, [])
+ const handleRemoveItem = useCallback(_item=> {setData(prevData =>{
+  const newData = prevData.filter(i=>i!==item)
+  return newData
+ })
+
+}, [])
+
+
   return (
     <Center
       _dark={{ bg: "blueGray.900" }}
@@ -38,6 +82,7 @@ export default function MainScreen(): JSX.Element {
       flex={1}
     >
       <VStack space={5} alignItems="center" w="full">
+      <TaskList />
         <TaskItem
           isDone={checked}
           onToggleCheckbox={handlePressCheckbox}
@@ -54,5 +99,4 @@ export default function MainScreen(): JSX.Element {
         <ThemeSwitch />
       </VStack>
     </Center>
-  );
-}
+  );}
