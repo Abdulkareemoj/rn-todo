6471275bc21ce43b9037f3e3446098b339f1006d
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-11-25 21:14:23 +0100

    add text and features

--- src/screens/about.tsx
+++ src/screens/about.tsx
@@ -1,7 +1,77 @@
 import React from "react";
-import { Box, Text, VSstack } from "native-base";
+import {
+  Box,
+  Text,
+  VStack,
+  Icon,
+  Image,
+  useColorModeValue,
+  ScrollView,
+} from "native-base";
+import { Feather } from "@expo/vector-icons";
+import ColorBox from "../components/color-box";
+import NavBar from "../components/navbar";
+import Masthead from "../components/masthead";
+import LinkButton from "../components/link-button";
+
 const AboutScreen = (): JSX.Element => {
-  return <Box>About</Box>;
+  return (
+    <ColorBox
+      flex={1}
+      bg={useColorModeValue("warmGray.50", "warmGray.900")}
+      w="full"
+    >
+      <Masthead title="About this app" image={require("../assets")}>
+        <NavBar />
+      </Masthead>
+      <ScrollView
+        borderTopLeftRadius="20px"
+        borderTopRightRadius="20px"
+        bg={useColorModeValue("warmGray", "primary.900")}
+        mt="-20px"
+        pt="30px"
+        p={4}
+      >
+        <VStack flex={1} space={4}>
+          <Box alignItems="center">
+            <Image
+              source={require("../assets")}
+              borderRadius="full"
+              resizeMode="cover"
+              w={120}
+              h={120}
+              alt="author"
+            />
+            <Text>This where i will put an About Text</Text>
+            <LinkButton
+              colorScheme="red"
+              size="lg"
+              borderRadius="full"
+              href="#"
+              leftIcon={
+                <Icon as={Feather} name="youtube" size="sm" opacity={0.5} />
+              }
+            >
+              {" "}
+              Go to link
+            </LinkButton>
+            <LinkButton
+              colorScheme="red"
+              size="lg"
+              borderRadius="full"
+              href="#"
+              leftIcon={
+                <Icon as={Feather} name="youtube" size="sm" opacity={0.5} />
+              }
+            >
+              {" "}
+              Go to link
+            </LinkButton>
+          </Box>
+        </VStack>
+      </ScrollView>
+    </ColorBox>
+  );
 };
 
 export default AboutScreen;
